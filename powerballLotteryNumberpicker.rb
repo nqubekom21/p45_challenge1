5e2a152b157a9b559f318a57885f32378ff84467
Nqubeko <nqubekom21@gmail.com>
2022-08-22 09:12:02 +0200

    ruby commit

--- powerballLotteryNumberpicker.rb
+++ powerballLotteryNumberpicker.rb
@@ -1,14 +1,13 @@
-array = Array.new
-puts "Enter the number of lines you want"
-input = gets.strip.chomp.to_i
-input.times do
-    5.times do
-        number = rand(1..50)
-        while array.include?(number) == true
-            number = rand(1..50)
-        end
-        array.push(number)
-    end
-    puts "#{array.join(" ")} bonus: #{rand(1..20)}"
-    array.clear
+def lotteryPowerball
+    powerball = rand(1..20)
+    array = Array.new(5){rand(0..50)}
+    #array.push(powerball)
+    puts "#{array.join(" ")} powerbal: #{powerball}"
 end
+    
+puts "Enter the number of lines: "
+input = gets.strip.chomp.to_i # number of lines
+
+input.times {
+    lotteryPowerball
+}def lotteryPowerball
